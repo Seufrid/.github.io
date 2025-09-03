7eff24bb0a5fd1c168f45ca443b0e311a0c81cbc
Seufrid <37951402+Seufrid@users.noreply.github.com>
2025-09-03 20:16:15 +0800

    Add files via upload

---
+++ js/iium-main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function ($) {
+    $(".hamburger").click(function (e) {
+        e.preventDefault();
+        $(this).next(".show-menu").slideToggle('slow');
+        $(".openanimate")[0].onclick();
+    });
+
+    $(".openanimate").click(function (e) {
+        $(".openanimate")[0].onclick();
+    });
+
+
+});
